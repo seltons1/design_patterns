54b8826b1bee320531d1c136da92e5045bd9a3a1
Elton Santos <seltons1@gmail.com>
2021-10-18 20:10:43 -0300

    Code improvement with strategy

--- calculate_tax.py
+++ calculate_tax.py
@@ -4,21 +4,16 @@ class calculate_tax(object):
 
     def calculate(self, budget, tax):
 
-        if tax =='ISS':
-            tax = calculate_iss(budget)
-            print(tax)
-        if tax == 'ICMS':
-            tax = calculate_icms(budget)
-            print(tax)
-    
+        tax = tax(budget)
+        print(tax)
+        
 if __name__ == '__main__':
 
     from budget import budget
     
-
     calc = calculate_tax()
 
     budget = budget(500)
     
-    calc.calculate(budget,'ISS')
-    calc.calculate(budget,'ICMS')
+    calc.calculate(budget,calculate_iss)
+    calc.calculate(budget,calculate_icms)
